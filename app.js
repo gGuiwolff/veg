dff082439dafcc4bd13e6910fc359fb03cc489c4
gGuiwolff <74604286+gGuiwolff@users.noreply.github.com>
2021-06-21 16:42:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -91,6 +91,10 @@ app.use("/teste", () => console.log('[EXPO AQUI]'))
 //app.use("/cloudimages")
 // FECHA TESTE====
 
+app.get("*", requireLoggedInUser, (req, res) =>
+    res.sendFile(path.join(__dirname,"/client/index.html"))
+);
+
 var port = process.env.PORT || 3000;
 server.listen(port, () => console.log("http://apivegiwe-com.umbler.net"));
 
