408626d579833438623941284208dad7ae759c66
gGuiwolff <74604286+gGuiwolff@users.noreply.github.com>
2021-06-21 19:46:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(express.static(path.join(__dirname, "client", "public")));
+app.use(express.static(path.join(__dirname, "client")));
 
 
 //Mount routers
