2c79d066c711001609e098ce77759f15bd0ce427
gGuiwolff <74604286+gGuiwolff@users.noreply.github.com>
2021-06-25 01:54:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const server = require("http").Server(app);
 const io = require("socket.io")(server, {
     allowRequest: (req, callback) =>
-        callback(null, req.headers.referer.startsWith("nwefront-aso5aoegn-gguiwolff.vercel.app")),
+        callback(null, req.headers.referer.startsWith("http://localhost:3000")),
 });
 const compression = require("compression");
 const path = require("path");
